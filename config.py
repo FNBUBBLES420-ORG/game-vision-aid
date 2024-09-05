48251db92cc38e7202ba5f33a0687f1e18d6c569
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-09-05 18:11:34 -0400

    Add files via upload
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

---
+++ config.py
@@ -0,0 +1,4 @@
+# Configuration for BetterCam Screen Capture and YOLO model
+screenWidth = 320  # Recommended screen width
+screenHeight = 320  # Recommended screen height
+confidenceThreshold = 0.5  # Confidence threshold for object detection
