de0337e72fbb346c967ca17b364ca1af1e3322f4
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-09-06 17:36:47 -0400

    Update install_pytorch_cpu_only.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- install_pytorch_cpu_only.bat
+++ install_pytorch_cpu_only.bat
@@ -2,7 +2,7 @@
 
 REM Install torch, torchvision, and torchaudio with +cpu option
 echo Installing torch, torchvision, and torchaudio with +cpu option...
-pip3 install torch+cpu torchvision+cpu torchaudio+cpu
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu/torch_stable.html
 
 REM Check if the installation was successful
 if %errorlevel% neq 0 (
