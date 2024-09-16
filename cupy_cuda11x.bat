164e5834fcc785c5d35eaa6992e4c46081a25f0a
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-09-16 19:26:58 -0400

    Create cupy_cuda11x.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

---
+++ cupy_cuda11x.bat
@@ -0,0 +1,13 @@
+@echo off
+echo MAKE SURE TO HAVE THE WHL DOWNLOADED BEFORE YOU CONTINUE!!!
+pause
+echo Click the link to download the WHL:
+echo https://github.com/cupy/cupy/releases/download/v12.0.0b1/cupy_cuda11x-12.0.0b1-cp311-cp311-win_amd64.whl
+pause
+
+echo Installing CuPy from WHL...
+pip install https://github.com/cupy/cupy/releases/download/v12.0.0b1/cupy_cuda11x-12.0.0b1-cp311-cp311-win_amd64.whl
+pause
+
+echo All packages installed successfully!
+pause
