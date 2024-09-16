c3b52b7c291362ac073c7e2141aefb2c39ced1e9
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-09-16 19:44:50 -0400

    Update requirements.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- requirements.bat
+++ requirements.bat
@@ -9,21 +9,6 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
-:: Initial CuPy installation prompt
-echo MAKE SURE TO HAVE THE WHL DOWNLOADED BEFORE YOU CONTINUE!!!
-pause
-echo Click the link to download the WHL: press ctrl then left click with mouse
-echo https://github.com/cupy/cupy/releases/download/v12.0.0b1/cupy_cuda11x-12.0.0b1-cp311-cp311-win_amd64.whl
-pause
-
-echo Installing CuPy from WHL...
-pip install https://github.com/cupy/cupy/releases/download/v12.0.0b1/cupy_cuda11x-12.0.0b1-cp311-cp311-win_amd64.whl
-if %errorlevel% neq 0 (
-    echo Failed to install CuPy from WHL
-    pause
-    exit /b 1
-)
-
 :: Install each package separately
 echo Installing opencv-python...
 pip install opencv-python
