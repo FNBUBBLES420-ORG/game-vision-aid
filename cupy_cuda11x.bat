f234fe50c62847473af8dfe328490ac4d835a786
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-09-16 19:30:40 -0400

    Update cupy_cuda11x.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- cupy_cuda11x.bat
+++ cupy_cuda11x.bat
@@ -1,7 +1,7 @@
 @echo off
 echo MAKE SURE TO HAVE THE WHL DOWNLOADED BEFORE YOU CONTINUE!!!
 pause
-echo Click the link to download the WHL:
+echo Click the link to download the WHL: press ctrl then left click with mouse
 echo https://github.com/cupy/cupy/releases/download/v12.0.0b1/cupy_cuda11x-12.0.0b1-cp311-cp311-win_amd64.whl
 pause
 
