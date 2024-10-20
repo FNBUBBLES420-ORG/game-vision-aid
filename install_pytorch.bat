45b160e48d6bf230a99aa2b0b2236b693cb1d231
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-10-19 22:16:30 -0400

    Update install_pytorch.bat
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

--- install_pytorch.bat
+++ install_pytorch.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Install PyTorch, TorchVision, and Torchaudio with CUDA 11.8 support
+REM Install PyTorch 2.5.0+cu118+cu118, TorchVision 0.20.0+cu118, and Torchaudio 2.5.0+cu118+cu118 with CUDA 11.8 support
 
 python --version >nul 2>&1
 if %errorlevel% neq 0 (
@@ -10,7 +10,7 @@ if %errorlevel% neq 0 (
 
 echo Installing PyTorch, TorchVision, and Torchaudio...
 
-pip3 install torch==2.4.1+cu118 torchvision==0.19.1+cu118 torchaudio==2.4.1+cu118 --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch==2.5.0+cu118 torchvision==0.20.0+cu118 torchaudio==2.5.0+cu118 --index-url https://download.pytorch.org/whl/cu118
 
 if %errorlevel% neq 0 (
     echo Installation failed.
